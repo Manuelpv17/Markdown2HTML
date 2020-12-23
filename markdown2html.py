5cc26c9438a4de22dad1ce4c957e3a48e6230e51
Manuelpv17 <manuelpv17@outlook.com>
2020-12-23 15:41:12 -0500

    Add task 1, headings

--- markdown2html.py
+++ markdown2html.py
@@ -6,17 +6,39 @@ import sys
 
 def markdownToHtml():
     if (len(sys.argv) < 3):
-        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
         sys.exit(1)
 
     markdownFile = sys.argv[1]
     outputFile = sys.argv[2]
 
     try:
-        f = open("{}".format(markdownFile))
+        with open("{}".format(markdownFile), "r") as f:
+            pass
     except IOError:
         print("Missing {}".format(markdownFile), file=sys.stderr)
         sys.exit(1)
+
+    headingsHtml = ["<h1>", "<h2>", "<h3>", "<h4>", "<h5>", "<h6>"]
+    headingsMarkdown = ["# ", "## ", "### ", "#### ", "##### ", "###### "]
+
+    text = ""
+    with open("{}".format(markdownFile), "r") as markdown:
+        with open("{}".format(outputFile), "w+") as html:
+            for line in markdown.read().split("\n"):
+                flag = 0
+                for i in range(len(headingsMarkdown) - 1, -1, -1):
+                    if headingsMarkdown[i] in line:
+                        text += line.replace(
+                            headingsMarkdown[i], headingsHtml[i])
+                        text += headingsHtml[i] + '\n'
+                        flag = 1
+                        break
+                if flag == 0:
+                    text += line + '\n'
+
+            html.write(text[:-1])
 
     sys.exit(0)
 
