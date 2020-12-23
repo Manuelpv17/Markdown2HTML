9bc0e146bef6fa6dfd84c46efa5764307e901aec
Manuelpv17 <manuelpv17@outlook.com>
2020-12-23 17:39:26 -0500

    Add task 3, ol

--- markdown2html.py
+++ markdown2html.py
@@ -20,37 +20,66 @@ def markdownToHtml():
         print("Missing {}".format(markdownFile), file=sys.stderr)
         sys.exit(1)
 
-    headingsHtml = ["<h1>", "<h2>", "<h3>", "<h4>", "<h5>", "<h6>"]
-    headingsHtmlEnd = ["</h1>", "</h2>", "</h3>", "</h4>", "</h5>", "</h6>"]
-    headingsMarkdown = ["# ", "## ", "### ", "#### ", "##### ", "###### "]
-    listMarkdown = ["- "]
-    listHtml = ["<ul>", "<li>"]
-    listHtmlEnd = ["</ul>", "</li>"]
-
     text = ""
-    ul_flag = 0
+    flag_ul = 0
+    flag_ol = 0
     with open("{}".format(markdownFile), "r") as markdown:
         with open("{}".format(outputFile), "w+") as html:
             for line in markdown.read().split("\n"):
-                for i in range(len(headingsMarkdown) - 1, -1, -1):
-                    if headingsMarkdown[i] in line:
-                        text += line.replace(
-                            headingsMarkdown[i], headingsHtml[i])
-                        text += headingsHtmlEnd[i] + '\n'
-                        break
-                if listMarkdown[0] in line:
-                    if ul_flag == 0:
-                        text += listHtml[0] + '\n'
-                    ul_flag = 1
-                    text += line.replace(listMarkdown[0], listHtml[1])
-                    text += listHtmlEnd[1] + '\n'
-                elif listMarkdown[0] not in line and ul_flag == 1:
-                    text += listHtmlEnd[0] + '\n'
-                    ul_flag = 0
+                text = titles(text, line)
+                text, flag_ul = ul(text, line, flag_ul)
+                text, flag_ol = ol(text, line, flag_ol)
 
             html.write(text)
 
     sys.exit(0)
+
+
+def titles(text, line):
+    headingsHtml = ["<h1>", "<h2>", "<h3>", "<h4>", "<h5>", "<h6>"]
+    headingsHtmlEnd = ["</h1>", "</h2>", "</h3>", "</h4>", "</h5>", "</h6>"]
+    headingsMarkdown = ["# ", "## ", "### ", "#### ", "##### ", "###### "]
+    for i in range(len(headingsMarkdown) - 1, -1, -1):
+        if headingsMarkdown[i] in line:
+            text += line.replace(
+                headingsMarkdown[i], headingsHtml[i])
+            text += headingsHtmlEnd[i] + '\n'
+            break
+    return text
+
+
+def ul(text, line, flag):
+    listMarkdown = ["- "]
+    listHtml = ["<ul>", "<li>"]
+    listHtmlEnd = ["</ul>", "</li>"]
+
+    if listMarkdown[0] in line:
+        if flag == 0:
+            text += listHtml[0] + '\n'
+        flag = 1
+        text += line.replace(listMarkdown[0], listHtml[1])
+        text += listHtmlEnd[1] + '\n'
+    elif listMarkdown[0] not in line and flag == 1:
+        text += listHtmlEnd[0] + '\n'
+        flag = 0
+    return text, flag
+
+
+def ol(text, line, flag):
+    listMarkdown = ["* "]
+    listHtml = ["<ol>", "<li>"]
+    listHtmlEnd = ["</ol>", "</li>"]
+
+    if listMarkdown[0] in line:
+        if flag == 0:
+            text += listHtml[0] + '\n'
+        flag = 1
+        text += line.replace(listMarkdown[0], listHtml[1])
+        text += listHtmlEnd[1] + '\n'
+    elif listMarkdown[0] not in line and flag == 1:
+        text += listHtmlEnd[0] + '\n'
+        flag = 0
+    return text, flag
 
 
 if __name__ == "__main__":
